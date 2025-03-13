398cccf2bd48297e105c78ae15590fc433218ea5
Jonathan Woodard <jwoodard@gmail.com>
2025-03-12 23:49:11 -0700

    Update plotting.py
    base function for a 3D scatter plot

--- src/plotting.py
+++ src/plotting.py
@@ -1,1 +1,32 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import plotly.express as px
+import plotly.graph_objs as go
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+def basic_3d(df):
+    data = [go.Scatter3d(
+        x=df['x'].values,
+        y=df['y'].values,
+        z=df['z'].values,
+        hoverinfo='text',
+        hoverlabel=dict(
+            bgcolor='cornsilk'
+            ),
+        hovertext=df['text'],
+        mode='markers',
+        marker=dict(
+            size=2,
+            color=df['colors']
+            )
+        )]
+    layout = go.Layout(
+        hovermode= 'closest',
+        showlegend= False
+        )
+    fig = go.Figure(data=data, layout=layout)
+    fig.write_html('temp.html', auto_open=False)
 
